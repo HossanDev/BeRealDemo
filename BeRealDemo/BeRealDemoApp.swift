f795d9696b883353fd0d3b2908e14dc908df01c8
MohammadHossan <hossan.dev@outlook.com>
2025-07-15 16:34:13 +0100

    Initial Commit

---
+++ BeRealDemo/BeRealDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealDemoApp.swift
+//  BeRealDemo
+//
+//  Created by MohammadHossan on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeRealDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
